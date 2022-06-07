e718eeeac28001499b3c3aef3c7d44dcb04e871f
Ben Guetta <95125332+bn-jo@users.noreply.github.com>
2022-06-08 01:57:58 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+const a = document.getElementById('1')
+const b = document.getElementById('2')
+const c = document.getElementById('3')
+const d = document.getElementById('4')
+const e = document.getElementById('5')
+const f = document.getElementById('6')
+const ad = document.getElementById('ad')
+const ad2 = document.getElementById('ad2')
+const ft = document.getElementById('ft')
+
+
+a.addEventListener('click',() => {
+    window.open('#');
+})
+
+b.addEventListener('click',() => {
+    window.open('#');
+})
+
+c.addEventListener('click',() => {
+    window.open('#');
+})
+
+d.addEventListener('click',() => {
+    window.open('#');
+})
+
+e.addEventListener('click',() => {
+    window.open('#');
+})
+f.addEventListener('click',() => {
+    window.open('#');
+})
+
+ad.addEventListener('click',() => {
+    window.open('https://www.bpinuk.co.il/');
+})
+
+ad2.addEventListener('click',() => {
+    window.open('https://www.bpinuk.co.il/');
+})
+ft.addEventListener('click',() => {
+    window.open('https://www.bpinuk.co.il/');
+})
