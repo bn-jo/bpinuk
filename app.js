9f3291d9a410e3ed8bb59b67d276f74288499f99
Ben Guetta <95125332+bn-jo@users.noreply.github.com>
2022-06-08 15:48:26 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,32 +4,40 @@ const c = document.getElementById('3')
 const d = document.getElementById('4')
 const e = document.getElementById('5')
 const f = document.getElementById('6')
+const g = document.getElementById('7')
+const h = document.getElementById('8')
 const ad = document.getElementById('ad')
 const ad2 = document.getElementById('ad2')
 const ft = document.getElementById('ft')
 
 
 a.addEventListener('click',() => {
-    window.open('#');
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/המסיבה-שתקפיץ-אתכם-R10-6-22-1080x1080-1.png');
 })
 
 b.addEventListener('click',() => {
-    window.open('#');
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/דובדבניםמסלול-מים2-1080x1080-לוואצאפ.png');
 })
 
 c.addEventListener('click',() => {
-    window.open('#');
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/1080x1080-sinni-%D7%A1%D7%99%D7%A0%D7%99-%D7%9C%D7%95%D7%95%D7%A6%D7%90%D7%A4.png');
 })
 
 d.addEventListener('click',() => {
-    window.open('#');
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/דובדבניםמסלול-מים2-1080x1080-לוואצאפ.png');
 })
 
 e.addEventListener('click',() => {
-    window.open('#');
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/המסיבה-שתקפיץ-אתכם17.6-Recovered-1080x1080-1.png');
 })
 f.addEventListener('click',() => {
-    window.open('#');
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/המסיבה-שתקפיץ-אתכם24-6-22-Recovered-1080x1080-1.png');
+})
+g.addEventListener('click',() => {
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/1080x1080-AdWhatsapp-photo-Taba-26-29-6-22-טאבה-לוואצאפ_.png');
+})
+h.addEventListener('click',() => {
+    window.open('https://www.bpinuk.co.il/wp-content/uploads/2022/06/1080-לווצאפ-ירדן-לווצאפ-Ad-Fb-photo-jorden-_.png');
 })
 
 ad.addEventListener('click',() => {
